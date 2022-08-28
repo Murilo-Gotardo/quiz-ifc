2ba98f4c95d214a07284ec623d84ec87cd10aaf4
murilo-gotardo <murilogotardo08@gmail.com>
2022-08-27 23:32:57 -0300

    Código de aleatorização das perguntas e respostas refeito

--- index.php
+++ index.php
@@ -25,70 +25,124 @@ require_once "src/ConexaoBD.php";
 
 <body>
     <main>
-        <?php
-        //Conecção com o banco de dados
-        $conexao = ConexaoBanco::getConexao();
-
-
-        ?>
         <div class="questao">
             <?php
-            $cont = 1;
-            $erroCont = 1;
-            for (;;) {
-                $i = rand(1, 3);
-                $per = $i;
 
+            //Conecção com o banco de dados
+            $conexao = ConexaoBanco::getConexao();
 
-                //Perguntas
-                $sqlPerguntas = "select pergunta, idPergunta from perguntas where idPergunta = " . $per;
-                $resultadoP = $conexao->query($sqlPerguntas);
-                $perguntas = $resultadoP->fetchAll(PDO::FETCH_ASSOC);
+            function excluiSelecionados(int $quantidade, int $maximo, array $excluidos): array
+            {
 
-                foreach ($perguntas as $pe) {
-                    echo "<p class='questao pergunta' id='" . $pe['idPergunta'] . "'>" . $pe['pergunta'] . "</p>";
+                // Cria o array inicial
+                $dicionario = range(1, $maximo);
+
+                // Exclui os inválidos
+                $dicionario = array_values(array_diff($dicionario, $excluidos));
+
+                $resultado = [];
+
+                // Gera a quantidade de número definidas
+                for ($i = 0; $i < $quantidade; $i++) {
+
+                    // Gera um número aletorio de 0 até a quantidade existente no dicionario.
+                    $id = random_int(0, count($dicionario) - 1);
+
+                    // Salva no "resultado"
+                    $resultado[] = $dicionario[$id];
+
+                    // Remove o número gerado da lista, afim de não gerar novamente o mesmo número
+                    unset($dicionario[$id]);
+                    $dicionario = array_values($dicionario);
                 };
 
-                //Respostas corretas
-                $sqlRespostasCorretas = "select respostaCorreta, idRespostaCorreta from respostaCorreta where idRespostaCorreta = " . $i;
-                $resultadoC = $conexao->query($sqlRespostasCorretas);
-                $corretas = $resultadoC->fetchAll(PDO::FETCH_ASSOC);
-
-                foreach ($corretas as $co) {
-                    echo "<p class='questao correta' id='" . $co['idRespostaCorreta'] . "'>" . $co['respostaCorreta'] . "<br></p>";
-                };
-
-                //Respostas incorretas
-                $sqlRespostasIncorretas = "select * from respostaIncorreta where idPergunta = " . $i;
-                $resultadoI = $conexao->query($sqlRespostasIncorretas);
-                $incorretas = $resultadoI->fetchAll(PDO::FETCH_ASSOC);
-
-                //Repete três vezes para imprimir as respostas erradas em ordem, de acordo com o "$erroCont"
-                for ($i = 1; $i <= 3; $i++) {
-                    foreach ($incorretas as $in) {
-                        echo "<p class='questao grupo-errado-" . $cont . "'>" . $in['respostaIncorreta' . $erroCont] . "<br></p>";
-                    };
-                    $erroCont++;
-
-                    //confirma se "$erroCont" ja foi acrescentado o suficiente antes de parar
-                    if ($erroCont == 4) {
-                        $erroCont = 1;
-                    };
-                }
-
-                $cont++;
-
-                if ($cont == 16) {
-                    break;
-                }
+                return $resultado;
             };
 
+            $cont = 0;
+            //Perguntas
+            $sqlPerguntas = "SELECT * FROM perguntas ORDER BY RAND() limit 3";
+            $resultadoP = $conexao->query($sqlPerguntas);
+            $perguntas = $resultadoP->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($perguntas as $pe) {
+                //Respostas Incorretas
+                $sqlRespostasIncorretas = "SELECT respostaIncorreta1, respostaIncorreta2, respostaIncorreta3 FROM perguntas WHERE idPergunta = " . $pe['idPergunta'] . " ORDER BY RAND() limit 3";
+                $resultadoIn = $conexao->query($sqlRespostasIncorretas);
+                $respostasIncorretas = $resultadoIn->fetchAll(PDO::FETCH_ASSOC);
+
+                //Impressão da pergunta
+                echo "<p class='questao pergunta' id='" . $pe['idPergunta'] . "'>" . $pe['pergunta'] . "</p>";
+
+                foreach ($respostasIncorretas as $reIn) {
+                    $idResIn = excluiSelecionados(3, 4, [4]);
+                    $ale = rand(1, 4);
+
+                    if ($ale == 1) {
+                        echo "<p class='questao' id='" . $pe['idPergunta'] . "'>" . $pe['respostaCorreta'] . "</p>";
+                        foreach ($idResIn as $num) {
+                            echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                        };
+                    } elseif ($ale == 2) {
+
+                        foreach ($idResIn as $num) {
+                            echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                            $cont++;
+                            if ($cont == 1) {
+                                for ($i = 0; $i < 1; $i++) {
+                                    echo "<p class='questao' id='" . $pe['idPergunta'] . "'>" . $pe['respostaCorreta'] . "</p>";
+                                }
+                            }
+                        };
+
+                        $cont = 0;
+                    } elseif ($ale == 3) {
+                        foreach ($idResIn as $num) {
+                            $cont++;
+
+                            if ($cont == 1) {
+                                echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                                if ($num == 1) {
+                                    $num = rand(2, 3);
+                                } elseif ($num == 3) {
+                                    $num = rand(1, 2);
+                                }
+
+                                echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+
+                                for ($i = 0; $i < 1; $i++) {
+                                    echo "<p class='questao' id='" . $pe['idPergunta'] . "'>" . $pe['respostaCorreta'] . "</p>";
+                                }
+
+                                if ($num != 2 && $num != 3) {
+                                    $num = 1;
+                                    echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                                } elseif ($num != 1 && $num != 3) {
+                                    $num = 2;
+                                    echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                                } elseif ($num != 1 && $num != 2) {
+                                    $num = 3;
+                                    echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                                }
+                            }
+                        };
+
+                        $cont = 0;
+                    } elseif ($ale == 4) {
+                        foreach ($idResIn as $num) {
+                            echo "<p class='questao'>" . $reIn['respostaIncorreta' . $num] . "</p>";
+                        };
+                        
+                        echo "<p class='questao' id='" . $pe['idPergunta'] . "'>" . $pe['respostaCorreta'] . "</p>";
+                    }
+                };
+            };
             ?>
         </div>
     </main>
     <script>
         let idPCru = document.getElementsByClassName("pergunta");
-        let idP =  idPCru.id;
+        let idP = idPCru.id;
 
         let idCCru = document.getElementsByClassName("correta");
         let idC = idCCru.id;
--- quizBD.sql
+++ quizBD.sql
@@ -23,12 +23,13 @@ DROP TABLE IF EXISTS `perguntas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `perguntas` (
-  `idPergunta` int NOT NULL AUTO_INCREMENT,
   `pergunta` varchar(800) DEFAULT NULL,
-  `idRespostaCorreta` int DEFAULT '1',
-  PRIMARY KEY (`idPergunta`),
-  KEY `idRespostaCorreta` (`idRespostaCorreta`),
-  CONSTRAINT `perguntas_ibfk_1` FOREIGN KEY (`idRespostaCorreta`) REFERENCES `respostaCorreta` (`idRespostaCorreta`)
+  `respostaIncorreta2` varchar(500) DEFAULT NULL,
+  `respostaIncorreta1` varchar(500) DEFAULT NULL,
+  `respostaIncorreta3` varchar(500) DEFAULT NULL,
+  `respostaCorreta` varchar(500) DEFAULT NULL,
+  `idPergunta` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idPergunta`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,61 +39,8 @@ CREATE TABLE `perguntas` (
 
 LOCK TABLES `perguntas` WRITE;
 /*!40000 ALTER TABLE `perguntas` DISABLE KEYS */;
-INSERT INTO `perguntas` VALUES (1,'O que significa PHP?',1),(2,'complete: ca-',2),(3,'qual é a certa?',3);
+INSERT INTO `perguntas` VALUES ('o que significa PHP','javali','c++','éééé','Hyper',1),('complete: ca-','ra','rne','re','nenhuma',2),('concorda?','nao','sim','talvez','vai embora, louco!!!!',3);
 /*!40000 ALTER TABLE `perguntas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `respostaCorreta`
---
-
-DROP TABLE IF EXISTS `respostaCorreta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `respostaCorreta` (
-  `idRespostaCorreta` int NOT NULL AUTO_INCREMENT,
-  `respostaCorreta` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`idRespostaCorreta`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `respostaCorreta`
---
-
-LOCK TABLES `respostaCorreta` WRITE;
-/*!40000 ALTER TABLE `respostaCorreta` DISABLE KEYS */;
-INSERT INTO `respostaCorreta` VALUES (1,'HyperText coiso'),(2,'fé'),(3,'esta é a errada');
-/*!40000 ALTER TABLE `respostaCorreta` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `respostaIncorreta`
---
-
-DROP TABLE IF EXISTS `respostaIncorreta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `respostaIncorreta` (
-  `idRespostaIncorreta` int NOT NULL AUTO_INCREMENT,
-  `respostaIncorreta1` varchar(500) DEFAULT NULL,
-  `respostaIncorreta2` varchar(500) DEFAULT NULL,
-  `respostaIncorreta3` varchar(500) DEFAULT NULL,
-  `idPergunta` int DEFAULT NULL,
-  PRIMARY KEY (`idRespostaIncorreta`),
-  KEY `idPergunta_idx` (`idPergunta`),
-  CONSTRAINT `idPergunta` FOREIGN KEY (`idPergunta`) REFERENCES `perguntas` (`idPergunta`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `respostaIncorreta`
---
-
-LOCK TABLES `respostaIncorreta` WRITE;
-/*!40000 ALTER TABLE `respostaIncorreta` DISABLE KEYS */;
-INSERT INTO `respostaIncorreta` VALUES (1,'erro louco','HPH','c++',1),(2,'outro louco','pé','tré',2),(3,'certa 1','certa 2','certa 3',3);
-/*!40000 ALTER TABLE `respostaIncorreta` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -112,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-17 22:32:56
+-- Dump completed on 2022-08-27 23:31:42
