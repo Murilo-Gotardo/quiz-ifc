40f55cd10e5d17393bcae90d1e5ab66f8bc9b12f
murilo-gotardo <murilogotardo08@gmail.com>
2022-10-02 00:39:22 -0300

    Movendo arquivos

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once "src/ConexaoBD.php";
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
